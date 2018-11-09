7fdcfa7627122b79ee761b8b3315918673070bc8
Jessica Kim <jkim00@stuy.edu>
2018-11-08 21:15:02 -0500

    added addWordDiagonal, fixed something in constructor

--- WordSearch.java
+++ WordSearch.java
@@ -7,9 +7,9 @@ public class WordSearch {
      *@param col is the starting width of the WordSearch
      */
   public WordSearch(int rows,int cols){
-  	data = new char[cols][rows];
-  	for (int i = 0; i < cols; i++) {
-  		for (int j = 0; j < rows; j++) {
+  	data = new char[rows][cols];
+  	for (int i = 0; i < rows; i++) {
+  		for (int j = 0; j < cols; j++) {
   			data[i][j] = '_';
   		}
   	}
@@ -91,6 +91,32 @@ public class WordSearch {
   		data[row + i][col] = word.charAt(i);
   	}
   	return true;
+  }
+
+  	/**Attempts to add a given word to the specified position of the WordGrid.
+   	*The word is added from top left to bottom right, must fit on the WordGrid,
+   	*and must have a corresponding letter to match any letters that it overlaps.
+   	*
+   	*@param word is any text to be added to the word grid.
+   	*@param row is the vertical locaiton of where you want the word to start.
+   	*@param col is the horizontal location of where you want the word to start.
+   	*@return true when the word is added successfully. When the word doesn't fit,
+   	*or there are overlapping letters that do not match, then false is returned.
+   	*/
+  public boolean addWordDiagonal(String word,int row, int col){
+   	if (row < 0 ||
+   			col < 0 ||
+   			row >= data.length ||
+   			col >= data[0].length ||
+   			word.length() + row > data.length ||
+   			word.length() + col > data[row].length) return false;
+   	for (int i = 0; i < word.length(); i++) {
+   		if (data[row + i][col + i] != '_' && data[row + i][col + i] != word.charAt(i)) return false;
+   	}
+   	for (int i = 0; i < word.length(); i++) {
+   		data[row + i][col + i] = word.charAt(i);
+   	}
+   	return true;
   }
 
 }
